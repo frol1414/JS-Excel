d6500920403f63b358cdda74e11916bbd0c174a5
frol1414 <frol1414@gmail.com>
2020-07-01 09:04:10 +0300

    add new methods

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,17 @@
 import {DomListener} from "@core/DomListener";
 
 export class ExcelComponent extends DomListener{
+
+    constructor($root, options = {}) {
+        super($root, options.listeners);
+        this.name = options.name || ''
+    }
     //Return component's template
     toHTML() {
         return ''
+    }
+
+    init() {
+        this.initDOMListeners()
     }
 }
